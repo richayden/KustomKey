baca11294154c5ad5a9e1b72fecb9d04f084af61
Richard Hayden <dadhayden@aol.com>
2015-08-13 19:34:16 -0400

    Initial Commit

---
+++ KKI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KKI
+//
+//  Created by Richard Hayden on 8/13/15.
+//  Copyright © 2015 richayden. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
